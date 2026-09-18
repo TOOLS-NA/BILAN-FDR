7524e89b7c22ee038ad986c7eab37eaad01f50c1
pierrenarb <psc95@free.fr>
2026-09-18 09:07:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Stratégie réseau d'abord : l'appli récupère toujours la dernière version
    quand il y a du réseau, et retombe sur le cache sinon.
    Incrémenter CACHE à chaque mise en ligne de index.html. */
-const CACHE = "bfr-v9";
+const CACHE = "bfr-v10";
 const COQUILLE = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", e => {
